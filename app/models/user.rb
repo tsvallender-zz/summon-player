759d2879b42c789763e58fadd0b783f89b849767
Trevor Vallender <tsv@tsv.me.uk>
2021-01-24 12:24:58 +0000

    Initial Devise setup

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  # Not using :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :lockable, :timeoutable
+end
