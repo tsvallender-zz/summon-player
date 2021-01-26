94cc78175223369cd774665a44e537dc2d2f694e
Trevor Vallender <tsv@tsv.me.uk>
2021-01-26 18:22:19 +0000

    testing without mail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,10 +16,10 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  #config.require_master_key = true
+  config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
-  # Apache or NGINX already handles this.
+  # Apache or NGINX already handles this.+
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress CSS using a preprocessor.
@@ -61,15 +61,15 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "summon_player_production"
 
   config.action_mailer.perform_caching = false
-  ActionMailer::Base.smtp_settings = {
-    :user_name => 'apikey',
-    :password => Rails..application.credentials.sendgrid[:api_key],
-    :domain => 'summonplayer.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-  }
+  # ActionMailer::Base.smtp_settings = {
+  #   :user_name => 'apikey',
+  #   :password => Rails..application.credentials.sendgrid[:api_key],
+  #   :domain => 'summonplayer.com',
+  #   :address => 'smtp.sendgrid.net',
+  #   :port => 587,
+  #   :authentication => :plain,
+  #   :enable_starttls_auto => true
+  # }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
