8d8159e2299f2d15e5e0e4cdbfada6c228e6cc1d
Trevor Vallender <tsv@tsv.me.uk>
2021-03-08 12:03:55 +0000

    Seeding database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Create main user
+user = User.new
+user.email = "user@summonplayer.com"
+user.password = 'foobar'
+user.password_confirmation = 'foobar'
+user.username = 'user'
+user.description = 'I am not a real person'
+user.save!
+
+# Generate more users
+10.times do |n|
+    user = User.new
+    user.email = "example-#{n+1}@summonplayer.com"
+    user.password = "password"
+    user.password_confirmation = "password"
+    user.username = Faker::Name.unique.name.gsub!(/[^\w_]/i, '').first(20)
+    user.description = "I am Example #{n+1}"
+    user.save!
+end
+
+# Generate tags
+dnd = Tag.create!(name: "dnd", description: "Dungeons & Dragons")
+kob = Tag.create!(name: "kidsonbikes", description: "Kids on Bikes")
+warhammer = Tag.create!(name: "40k", description: "Warhammer 40000")
+
+users_subset = User.order(:created_at).take(6)
+
+# Generate ads
+users_subset.each do |user|
+    10.times do
+        ad = user.ads.create!(title: Faker::Lorem.sentence(word_count: 3).first(30),
+            text: "I need some people to play D&D with",
+            category: 'rpg')
+        AdTag.create!(ad_id: ad.id, tag_id: dnd.id)
+    end
+end
