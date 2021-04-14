aa13d26e329e39de2a3f3191be3407524deebe75
Trevor Vallender <tsv@tsv.me.uk>
2021-04-14 10:44:02 +0100

    chat updated_at updated for new messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,8 +4,15 @@ class Message < ApplicationRecord
 
   validates :text, presence: true, length: {maximum: 500} 
   validates :from, presence: true
+
+  after_save :touch_chat
   
   def to_param
     from.name
+  end
+
+  # Keep chat updated_at time correct with new messages
+  def touch_chat
+    chat.touch
   end
 end
