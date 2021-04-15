3e31105111bd9e0cd4e3fb694b0a11f74a475286
Trevor Vallender <tsv@tsv.me.uk>
2021-04-15 09:39:36 +0100

    refactored chat being touched by message, test currently fails

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,18 +1,11 @@
 class Message < ApplicationRecord
   belongs_to :from, class_name: "User"
-  belongs_to :chat
+  belongs_to :chat, touch: true
 
   validates :text, presence: true, length: {maximum: 500} 
   validates :from, presence: true
-
-  after_save :touch_chat
   
   def to_param
     from.name
-  end
-
-  # Keep chat updated_at time correct with new messages
-  def touch_chat
-    chat.touch
   end
 end
---
+++ test/integration/messages_chats_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class MessagesChatsTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+  def setup
+    @user = users(:trevor)
+    @bob = users(:bob)
+    @chat = chats(:one)
+  end
+
+  test "sending a message should update chats updated timestamp" do
+    sign_in @user
+    puts @chat.updated_at
+    assert_changes "@chat.updated_at" do
+      post messages_path(params: { message: { 
+        text: 'Hello', to_id: @bob.id, chat_id: @chat.id
+        }})
+      assert_response :success
+    end
+  end
+end
