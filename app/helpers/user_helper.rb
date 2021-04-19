17c7acc70117988e0d5b052435f5dd26c7ee5561
Trevor Vallender <tsv@tsv.me.uk>
2021-04-19 10:43:43 +0100

    Added _user render, resize avatars

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,9 +1,9 @@
 module UserHelper
-    def avatar(user)
+    def avatar(user, size)
         if user.image.attached?
-            image_tag(user.image.variant(resize_to_limit: [200, 200]))
+            image_tag(user.image.variant(resize_to_limit: [size, size]))
         else
-            image_tag('meeple.jpg')
+            image_tag('meeple.jpg', size: size.to_s)
         end
     end
 end
