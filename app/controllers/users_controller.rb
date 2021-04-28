e5c69bae08c0f66a4c540dea01e1339bc6c540fc
Trevor Vallender <tsv@tsv.me.uk>
2021-04-28 13:00:40 +0100

    CSS upgrades

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find_by(username: params[:id])
-    @ads = @user.ads.paginate(page: params[:page])
-    @chat = Chat.new # OR EXISTING CHAT!!!
+    @ads = @user.ads.paginate(page: params[:page]).order(created_at: :desc)
+    @chat = Chat.new
   end
 end
