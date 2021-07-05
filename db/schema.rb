0e6c37414329c21afa7254f563068dd480e0668f
Trevor Vallender <tsv@tsv.me.uk>
2021-07-05 10:42:01 +0100

    Created group and group_user models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_28_091541) do
+ActiveRecord::Schema.define(version: 2021_07_05_093604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -85,6 +85,24 @@ ActiveRecord::Schema.define(version: 2021_06_28_091541) do
     t.index ["subject_type", "subject_id"], name: "index_chats_on_subject_type_and_subject_id"
   end
 
+  create_table "group_users", force: :cascade do |t|
+    t.bigint "group_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["group_id", "user_id"], name: "index_group_users_on_group_id_and_user_id", unique: true
+    t.index ["group_id"], name: "index_group_users_on_group_id"
+    t.index ["user_id"], name: "index_group_users_on_user_id"
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_groups_on_user_id"
+  end
+
   create_table "messages", force: :cascade do |t|
     t.text "text"
     t.integer "from_id"
@@ -133,5 +151,8 @@ ActiveRecord::Schema.define(version: 2021_06_28_091541) do
   add_foreign_key "ads", "users"
   add_foreign_key "chat_users", "chats"
   add_foreign_key "chat_users", "users"
+  add_foreign_key "group_users", "groups"
+  add_foreign_key "group_users", "users"
+  add_foreign_key "groups", "users"
   add_foreign_key "messages", "chats"
 end
